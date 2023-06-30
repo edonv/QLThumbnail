0469284d68e377a9a15d7a31aea3c79b2e8c49ac
Edon <edon@valdman.works>
2023-06-30 12:43:17 -0500

    Update QLThumbnailTests.swift
    Commented-out no-longer-relevant boilerplate test code.

--- Tests/QLThumbnailTests/QLThumbnailTests.swift
+++ Tests/QLThumbnailTests/QLThumbnailTests.swift
@@ -6,6 +6,6 @@ final class QLThumbnailTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(QLThumbnail().text, "Hello, World!")
+//        XCTAssertEqual(QLThumbnail().text, "Hello, World!")
     }
 }
