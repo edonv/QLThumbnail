e111e1427c3945fbf222e2b8a2ce200f6b46949f
Edon <edon@valdman.works>
2023-06-30 12:42:26 -0500

    Update Package.swift
    Updated Package info.

--- Package.swift
+++ Package.swift
@@ -1,10 +1,15 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "QLThumbnail",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v11),
+        .macCatalyst(.v14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -13,14 +18,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/shaps80/SwiftUIBackports.git", .upToNextMajor(from: "2.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "QLThumbnail",
-            dependencies: []),
+            dependencies: [.byNameItem(name: "SwiftUIBackports", condition: nil)]),
         .testTarget(
             name: "QLThumbnailTests",
             dependencies: ["QLThumbnail"]),
